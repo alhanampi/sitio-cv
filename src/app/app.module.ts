44eb86f016791c8a888b7b681f81c07b964b5942
unknown <alhanampi@gmail.com>
2019-07-30 15:54:54 -0300

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -8,12 +9,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
 import { PhotosComponent } from './components/photos/photos.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ReposComponent } from './components/repos/repos.component';
 
 const routes:Routes = [
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "home", component: MainComponent},
   {path: "photos", component:  PhotosComponent},
   {path: "contact", component: ContactComponent},
+  {path: "github", component: ReposComponent},
 ]
 
 
@@ -24,10 +27,12 @@ const routes:Routes = [
     FooterComponent,
     MainComponent,
     PhotosComponent,
-    ContactComponent
+    ContactComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(
       routes, {enableTracing: !true}
     )
